057d870403e1f0617315725bc46f4099a9db470a
ananthvyayavaram07 <ananth.vyayavaram@senecaglobal.com>
2024-02-02 11:40:40 +0530

    config file added

--- tests/simple.test.ts
+++ tests/simple.test.ts
@@ -2,6 +2,7 @@ import test from "../support/utils/pageObjects";
 
 test.describe("",async() =>{        
     test("login", async ({page}) => {    
-        await page.goto("https://www.google.com");                        
+        await page.goto("https://www.google.com");      
+        console.log("Success")                  
     })
 })
